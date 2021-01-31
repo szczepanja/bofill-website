29c8118f11965aa30b1727ab8baede8008347691
Szczepanik Anja <szczepanikanja@gmail.com>
2021-01-31 22:16:05 +0100

    walden/la-muralla

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function goBack() {
-  window.history.back();
-}
+const data = {
+  english: {
+    title: "BOFILL'S ARCHITECTURE",
+    projects: "PROJECTS",
+  },
+  polish: {
+    title: "ARCHITEKTURA BOFILL'A",
+    sunbavbtn projects: "PROJEKTY",
+  },
+};
